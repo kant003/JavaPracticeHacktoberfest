dc76206c9dacead883e91b182345a32a13cb173b
ChinthakaFG <chinthaka@fortunaglobal.com>
2016-10-27 23:55:43 +0530

    Dynamic RGB Color

--- src/com/hacktoberfest/DynamicRGBColor.java
+++ src/com/hacktoberfest/DynamicRGBColor.java
@@ -8,5 +8,43 @@ package com.hacktoberfest;/*
 // ****************************************************************************************************
 */
 
+import java.awt.*;
+
 public class DynamicRGBColor {
+
+    public DynamicRGBColor() {
+        Color dynamicColor = generateDynamicColor();
+        System.out.println(dynamicColor.toString());
+    }
+
+    /**
+     * Generate dynamic color
+     * @return Color
+     */
+    private Color generateDynamicColor() {
+
+        int R = randomWithRange(0, 255);
+        int G = randomWithRange(0, 255);
+
+        int B = randomWithRange(0, 255);
+        return new Color(R,G,B);
+
+    }
+
+    /**
+     * RGB Max Value is 255 Min Value is 0, So here its getting min max value to generate RGB value.
+     * @param min
+     * @param max
+     * @return int
+     */
+    private int randomWithRange(int min, int max)
+    {
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
+
+    public static void main(String[] args) {
+        new DynamicRGBColor();
+    }
+
 }
