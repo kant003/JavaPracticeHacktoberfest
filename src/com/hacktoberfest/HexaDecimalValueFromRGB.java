de721f6b07246f4ef1c27053c820fee5a81ca3cb
ChinthakaFG <chinthaka@fortunaglobal.com>
2016-10-27 23:53:58 +0530

    Hexadecimal from RGB

--- src/com/hacktoberfest/HexaDecimalValueFromRGB.java
+++ src/com/hacktoberfest/HexaDecimalValueFromRGB.java
@@ -8,5 +8,33 @@ package com.hacktoberfest;/*
 // ****************************************************************************************************
 */
 
+import java.awt.*;
+
 public class HexaDecimalValueFromRGB {
+
+    public HexaDecimalValueFromRGB() {
+
+        Color color = Color.CYAN;
+        String hexValue = getHexValueFromColor(color);
+        System.out.println(hexValue);
+    }
+
+    /**
+     * This method will generate the HEXA value from RGB value
+     * @param color
+     * @return
+     */
+    private String getHexValueFromColor(Color color) {
+
+        int R = color.getRed();
+        int G = color.getGreen();
+        int B = color.getBlue();
+
+        String hex = String.format("#%02x%02x%02x", R, G, B);
+        return hex;
+    }
+
+    public static void main(String[] args) {
+        new HexaDecimalValueFromRGB();
+    }
 }
