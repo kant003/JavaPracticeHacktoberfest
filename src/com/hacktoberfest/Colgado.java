526292a16a35a8ab1034cf5772b7bc10d22afee7
Charlye <costassolla@gmail.com>
2016-10-27 13:53:10 +0200

    Programa colgado

---
+++ src/com/hacktoberfest/Colgado.java
@@ -0,0 +1,15 @@
+package com.hacktoberfest;
+
+public class Colgado {
+
+	public Colgado() {
+		// TODO Auto-generated constructor stub
+
+		boolean colgado = true;
+
+		while (colgado) {
+			System.out.println("Esto no para");
+		}
+	}
+
+}
