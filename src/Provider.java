5033eb0e213c5943c650c12f5aa64ad0ebc9f365
Angel Gonzalez <gonzalezm.angel@gmail.com>
2016-10-28 09:38:50 +0200

    add provider model

---
+++ src/Provider.java
@@ -0,0 +1,59 @@
+package com.cebem.model;
+
+public class Provider {
+
+	int id;
+	String name;
+	String address;
+	String email;
+
+	public Provider(int id, String name, String address, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.email = email;
+	}
+
+	public Provider() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
