a7b48d43c91b7ac4c4bf4e55f822e2e515fb08fb
Noelia <43953714+noeliabouzon@users.noreply.github.com>
2018-10-11 09:20:15 +0200

    Create Noelia2.java
    Un sumatorio de números.

---
+++ Noelia2.java
@@ -0,0 +1,11 @@
+public static void main(String [] args)
+	{
+	
+	int num=5;
+	num += num - 1 * 4 + 1;
+	System.out.println(num);
+	num=4;
+	num %= 7 * num % 3 * 7;
+	System.out.println(num);
+	}
+}
