ac84a03a8e94c12d6dc69b39ad83df4ee8b56c66
Bee <32143171+BeeCtown@users.noreply.github.com>
2018-10-11 13:31:50 +0200

    Create BeesCalculator.java

---
+++ BeesCalculator.java
@@ -0,0 +1,55 @@
+//untested :D
+
+import java.util.Scanner;
+
+public class Calculator
+{
+public static void main(String args[])
+{
+    float a, b, res;
+    char select, ch;
+    Scanner scan = new Scanner(System.in);
+
+    do
+    {
+        System.out.print("(1) Addition\n");
+        System.out.print("(2) Subtraction\n");
+        System.out.print("(3) Multiplication\n");
+        System.out.print("(4) Division\n");
+        System.out.print("(5) Exit\n\n");
+        System.out.print("Enter Your Choice : ");
+        choice = scan.next().charAt(0);
+
+        switch(select)
+        {
+            case '1' : System.out.print("Enter Two Number : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a + b;
+                System.out.print("Result = " + res);
+                break;
+            case '2' : System.out.print("Enter Two Number : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a - b;
+                System.out.print("Result = " + res);
+                break;
+            case '3' : System.out.print("Enter Two Number : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a * b;
+                System.out.print("Result = " + res);
+                break;
+            case '4' : System.out.print("Enter Two Number : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a / b;
+                System.out.print("Result = " + res);
+                break;
+            case '5' : System.exit(0);
+                break;
+            default : System.out.print("Wrong Choice!!!");
+        }
+    }while(choice != 5);       
+}
+}
