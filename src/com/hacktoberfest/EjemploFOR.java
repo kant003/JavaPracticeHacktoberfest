f48ba60768422bfdacd95f3cf93319818e6840c3
chichifu <manuel.couseloalonso@gmail.com>
2018-10-11 14:06:50 +0200

    Ejemplo bucle FOR

---
+++ src/com/hacktoberfest/EjemploFOR.java
@@ -0,0 +1,13 @@
+package com.hacktoberfest;
+
+public class EjemploFOR {
+
+    public static void main(String[] args) {
+        int i;
+
+        for(i=0; i<=20; i++) {
+            System.out.println("Soy la variable "+i+ "mi valor en esta iteracion es: %d\n"+i);
+        }
+
+    }
+}
