96d6c9b48837fa95818cc70713a1634a33e364d9
AfreenKhan777 <71901755+AfreenKhan777@users.noreply.github.com>
2020-10-02 19:54:39 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,30 +1,16 @@
-
-
 public class FizzBuzz
 {
     public static void main(String args[]) {
-        for (int i=1; i <= 100; i++){
-
+        for (int i=1; i <= 100; i++)
+        {
             if((i % 3 == 0) &&(i % 5 == 0))
-            {
                 System.out.println("fizz buzz");
-
-            }
             else if(i % 3 == 0)
-            {
                 System.out.println("fizz");
-
-            }
             else if(i % 5 == 0)
-            {
-
                 System.out.println("buzz");
-
-            }
             else
-            {
                 System.out.println (i);
-            }
         }
     }
 }
