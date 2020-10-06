35f930eb8d6e5353bed575ef958fee885cfc3174
AdayLourido <adaylourido@gmail.com>
2020-10-06 14:02:15 +0200

    Programa demasiado util

---
+++ ProgramaParaContarOvejas.java
@@ -0,0 +1,15 @@
+package Inferfacestema1;
+
+public class ProgramaParaContarOvejas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ovejas=0;
+		
+		for (int i = 0; i < 5000; i++) {
+			ovejas++;
+			System.out.println("Ovejas contadas: "+ovejas);
+		}
+	}
+
+}
