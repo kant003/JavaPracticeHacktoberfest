bf3a6b73932eb34f5f0e1e73020972a8753dc8ae
AdayLourido <adaylourido@gmail.com>
2020-10-06 14:14:05 +0200

    Ultimo

---
+++ OtromejorPrograma.java
@@ -0,0 +1,13 @@
+package Inferfacestema1;
+
+public class OtromejorPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 5000; i++) {
+			
+			System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		}
+	}
+
+}
