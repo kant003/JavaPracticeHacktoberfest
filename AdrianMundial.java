033e8f591cc5e722d5f01c9846e059464d47b0d0
AdriQQ <adrian.quiroga.quijano@gmail.com>
2020-10-06 14:04:14 +0200

    Un programa conocido

---
+++ AdrianMundial.java
@@ -0,0 +1,11 @@
+package pruebasjava;
+
+public class JavaPruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola amigos");
+
+	}
+
+}
