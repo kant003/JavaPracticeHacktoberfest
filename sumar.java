75453513e2c93c3cfb89572e1e4e9c9cfb4aa3ef
gunter895 <miriam.negrito@gmail.com>
2020-10-06 13:44:57 +0200

    programa para sumar 2 numeros

---
+++ sumar.java
@@ -0,0 +1,22 @@
+class sumar{
+
+  public static void main( String[] args )
+    {
+        int n1, n2, suma;
+
+        Scanner teclado = new Scanner( System.in );
+
+        System.out.print( "Introduzca primer número: " );
+        n1 = teclado.nextInt();
+
+        System.out.print( "Introduzca segundo número: " );
+        n2 = teclado.nextInt();
+
+        suma = n1 + n2;
+
+        System.out.println( "La suma de " + n1 + " más " + n2 + " es " + suma + "." );
+    }
+
+
+
+}
