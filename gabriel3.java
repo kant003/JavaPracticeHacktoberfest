c1638d4866d4b950085e61155348578f96497d94
gaborandom <gabrielnogueirarosales15@gmail.com>
2020-10-06 14:05:43 +0200

    tercer programa

---
+++ gabriel3.java
@@ -0,0 +1,43 @@
+package Ejercicios;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Ejercicio14Media {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ejercicio14Media window = new Ejercicio14Media();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Ejercicio14Media() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
