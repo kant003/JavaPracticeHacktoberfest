8dc49a46b88111d939c4ca2e02e1cdb13193a1cb
Samuel <bdzsamuel@gmail.com>
2020-10-06 12:06:09 +0200

    Versión ya sin KISS porque ha sido un desastre. WIP v0.2

--- SamuelHacktoberfest/src/samuel/chistes/Main.java
+++ SamuelHacktoberfest/src/samuel/chistes/Main.java
@@ -6,8 +6,19 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("INDICA QUE ARCHIVO QUIERES VER [Refranes (por defecto) / Chistes]:\n");
+		Scanner input = new Scanner(System.in);
+		readFile(input.next());
+		input.close();
+	}
+
+	public static void readFile(String tipo) {
 		try {
-			Scanner file = new Scanner(new File("./res/chistes.txt"));
+			String file_name = (tipo.equalsIgnoreCase("chistes") || tipo.equalsIgnoreCase("chiste"))
+					? "./res/chistes.txt"
+					: "./res/refranes.txt"; // TODO encontrar una manera más elegante
+			Scanner file = new Scanner(new File(file_name));
+
 			while (file.hasNextLine()) {
 				System.out.println(file.nextLine());
 			}
