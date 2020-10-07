b4f5360bb3c670f143735c4a09214cb8fe9a4968
AfreenKhan777 <71901755+AfreenKhan777@users.noreply.github.com>
2020-10-08 00:52:29 +0530

    Update charliefil.java

--- charliefil.java
+++ charliefil.java
@@ -1,8 +1,9 @@
 class charliefil {
-   public static void public static void main(String[] args) {
+   public static void main(String[] args) {
        
-  
+   int num1,num2,suma;
     num1= 2; num2=1;
     suma=num1+num2;
+      System.out.println(suma);
 }
 }
