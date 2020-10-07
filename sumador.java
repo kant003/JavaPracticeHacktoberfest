5a8128f02ded006d3dfbccf6a585a207868f29cf
AfreenKhan777 <71901755+AfreenKhan777@users.noreply.github.com>
2020-10-08 01:15:07 +0530

    Update sumador.java

--- sumador.java
+++ sumador.java
@@ -1,12 +1,13 @@
+import java.util.*;
 class sumador{
-
+   public static void main(String[] args){
     int numero1 = 0;
     int numero2 = 0;
-	
+	Scanner reader=new Scanner(System.in);	
     System.out.println("Introduce el primer número:");			
     numero1 = reader.nextInt();
 			
     System.out.println("Introduce el segundo número:");
     numero2 = reader.nextInt();
-
+   }
 }
