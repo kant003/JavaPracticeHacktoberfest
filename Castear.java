ea8762d206dd7a28f04d94e91f25153c26850cfc
abelgarcia2 <abelgg2000@gmail.com>
2020-10-07 18:26:09 +0200

    Un sencillo programa que castea un dato

---
+++ Castear.java
@@ -0,0 +1,13 @@
+
+public class casteo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int idato=5;
+		byte bdato;
+		bdato = (byte)idato;
+		System.out.println(bdato);
+	}
+
+}
