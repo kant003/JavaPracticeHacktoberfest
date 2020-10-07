6117b4e759051d0d671f85c6a92fef18405b8fbb
abelgarcia2 <abelgg2000@gmail.com>
2020-10-07 18:28:23 +0200

    Muestra por pantalla todos los numeros pares hasta el 100

---
+++ CalculaNumPares.java
@@ -0,0 +1,12 @@
+
+public class NumPares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0; i<=100; i+=2) {
+			System.out.println(i);
+		}
+	}
+
+}
