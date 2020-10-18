4a2e3f9fbe78e5636faf95bbc5e7c9d85432fd43
Kryss26 <yosoycris2010@gmail.com>
2020-10-18 17:16:12 +0200

    nuevo contenido en repositorio

---
+++ ProgramasJava/while.java
@@ -0,0 +1,10 @@
+public class while {
+    Run|Debug
+    public static void main(String[] args) { 
+
+        int contador;
+        contador = 1;
+        while(contador <= 10){
+            System.out.println(contador);
+        }
+}
