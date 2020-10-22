6f805e02934f299e540ff5555bf91cfee8b9f915
FJLEspinha <florenzoespina@gmail.com>
2020-10-22 11:31:08 +0200

    añado un nuevo pull requests

---
+++ Resta.java
@@ -0,0 +1,9 @@
+class Resta{
+    public static void main(String[] args){
+      int num1 = 5;
+      int num2 = 6;
+      int resta;
+      resta = num1 - num2;
+      System.out.println("La suma es: "+resta);
+    }
+  }
