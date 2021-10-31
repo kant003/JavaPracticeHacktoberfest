fd311475c2690ac22aa3540b7b6e4a1d31bd6860
Angel Gonzalez <gonzalezm.angel@gmail.com>
2021-10-31 22:56:03 +0100

    Create hol.js

---
+++ hol.js
@@ -0,0 +1,21 @@
+socket.on('rotaIzq', (direccion) => {
+        const vel = -18
+        const jugador = jugadores.get(socket.id)
+        jugador.vel.rota(vel)
+    })
+
+    socket.on('rotaDer', (direccion) => {
+        const vel = 18
+        const jugador = jugadores.get(socket.id)
+        jugador.vel.rota(vel)
+    })
+
+    socket.on('aumentaVel', (direccion) => {
+        const jugador = jugadores.get(socket.id)
+        jugador.acelera(1.5)
+    })
+
+    socket.on('disminuyeVel', (direccion) => {
+        const jugador = jugadores.get(socket.id)
+        jugador.desAcelera(1.5)
+    })
