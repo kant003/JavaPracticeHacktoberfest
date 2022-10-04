22677eb6af5ed65a9afecd609d3289bb60229bd4
Gabriel Romero <gabrielr6803@gmail.com>
2022-10-04 11:40:10 +0200

    añado .java

---
+++ GabrielRomero.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class GabrielRomero {
+    
+    //1 euro = 0,96 dolares
+	public static double  euroADolar(double e) {
+		
+		double temp=0;
+		temp = e*0.96 ;
+		return temp;
+	}
+	
+	//1 dolar= 1,05 euros
+	public static double dolarAEuro(double d) {
+		
+		double temp=0;
+		temp =  d*1.05;
+		return temp;
+	}
+
+	public static void main(String[] args) {
+		
+		double aux;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("por favor introduzca euros: ");
+		aux=sc.nextDouble();
+		System.out.println("en dolares son: "+euroADolar(aux));
+		
+		System.out.println("por favor introduzca dolares: ");
+		aux=sc.nextDouble();
+		System.out.println("en euros son: "+dolarAEuro(aux));
+
+	}
+}
