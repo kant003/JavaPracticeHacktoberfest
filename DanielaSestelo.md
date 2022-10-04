6ffbbdee420c8797780d405bbbff87a0836c2158
Danielasd20 <danielasestelo42@gmail.com>
2022-10-04 10:56:29 +0200

    pequeño programa de exponentes

---
+++ DanielaSestelo.md
@@ -0,0 +1,15 @@
+Mis pasos en esta calle
+
+Resuenan
+
+En otra calle
+
+Donde
+
+Oigo mis pasos
+
+Pasar en esta calle
+
+Donde
+
+Sólo es real la niebla.
---
+++ Exp.java
@@ -0,0 +1,9 @@
+public class Exp {
+    public static void main(String[] args) {
+        int base=9;
+        int exponente=2;
+        System.out.printl("El resultado es: 9^2= "+Math.pow(9, 2));
+
+    }
+    
+}
