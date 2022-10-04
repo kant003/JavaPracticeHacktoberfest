7ae8f89a69bccf519d4f62ebda26265c6419da40
Javier González <javigongimenez@gmail.com>
2022-10-04 12:04:12 +0200

    añado programa

---
+++ JavierGonzalezGimenez.java
@@ -0,0 +1,22 @@
+public class suma {
+	
+	static int n1 = 50; // variable miembro de la clase
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	/*int n2 = 30, suma= 0; // variables locales
+	
+	suma =n1 + n2;
+	
+	System.out.println("LA SUMA ES: " + suma);*/
+		
+		public void metodofake() {
+			System.out.println("fake");
+			System.out.println(n1);
+			}
+
+        
+}
---
+++ JavierGonzalezGimenez.md
@@ -0,0 +1,5 @@
+Tras tres tragos y otros tres, 
+y otros tres tras los tres tragos, trago y trago son estragos, 
+travesuras de entremés, trapola tramo y tragón, 
+treinta y tres tragos de ron tras trozos de trucha extremo, 
+en un tris los truene el trueno.
