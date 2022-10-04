61b3758ea9ff8c84656c3c39624886378054dd9e
Rafael Costas <rafaelcostas728@gmail.com>
2022-10-04 10:53:08 +0200

    Algo

---
+++ Suma.java
@@ -0,0 +1,10 @@
+class Suma{
+   
+    public static void main(String[] args){
+        int i=0;
+        int a=1;
+        int res=a+i;
+        System.out.println("El resultado de "+a+" y "+i+" es "+res);
+        
+    }
+}
