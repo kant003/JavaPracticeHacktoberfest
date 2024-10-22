0e4b1f4285c89f6cda2ec5b8aaf429d972405735
Diego-Delta <dalonso306@gmail.com>
2024-10-22 13:18:56 +0200

    ahora si es la 1º reques, creo

--- DiegoAlonso.java
+++ DiegoAlonso.java
@@ -2,7 +2,23 @@ import java.util.Scanner;
 
 public class DiegoAlonso {
 
+	public static void main(String[] args) {
 
+		int s;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("dime los segundos que quieras");
+		s = sc.nextInt();
+		System.out.println("se mostrarán" + s + " segsundos en dias, horas minutos y segundos");
+		
+		int h = s / 3600;
+		int m = s / 60;
+		int minus = m - (h * 60);
+		int res = s%60;
+		int dias= h/24;
+		int horas= h-(dias*24);
+		System.out.println("es "+dias+" dias " + horas + " horas " + minus + " minutos y " + res + " segundos");
+
+	}
     
   
 }
