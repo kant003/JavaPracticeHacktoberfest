8454542cddac365c27c746ca0121d8eb38a2806c
BraisK <braiscasares26@gmail.com>
2024-10-24 10:37:29 +0200

    Programa en Java

---
+++ braiscasares.java
@@ -0,0 +1,10 @@
+public class braiscasares{
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4, 5};
+
+       for (int numero : a) {
+            System.out.println("El " + (numero+1) + " º es " + numero);
+        }
+    }
+}
