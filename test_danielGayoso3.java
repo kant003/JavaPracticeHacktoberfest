fb05a823e618af356760e869b08bad2fa8450282
DanielCebem <danielfgayoso@gmail.com>
2024-10-25 11:46:07 +0200

    cambio3

---
+++ test_danielGayoso3.java
@@ -0,0 +1,8 @@
+public class test_propuesto11 {
+    public static void main(String[] args) {
+        Propuesto11 rest1 = new Propuesto11();
+        rest1.pedirClientes();
+        rest1.pedirProductos();
+        rest1.personasAlimentar();
+    }
+}
